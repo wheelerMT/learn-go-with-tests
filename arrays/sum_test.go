1c3b977c80d59ec87a939f881ff28fda247c7850
Matt Wheeler <wheelermt@proton.me>
2025-02-08 16:36:29 +0000

    Remove empty lines

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -39,14 +39,12 @@ func TestAllTails(t *testing.T) {
 	t.Run("make the sum of some slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
-
 		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
 		checkSums(t, got, want)
 	})
 }

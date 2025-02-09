e71feb8f45e738e6579df1ad21e4d30d57145c30
Matt Wheeler <wheelermt@proton.me>
2025-02-09 15:21:10 +0000

    Add Bitcoin class in addition to error handling of Withdraw function

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,33 @@
 package pointers
 
-type Wallet struct {
-	balance int
-}
+import (
+	"errors"
+	"fmt"
+)
 
-func (w *Wallet) Deposit(amount int) {
+type (
+	Bitcoin int
+	Wallet  struct {
+		balance Bitcoin
+	}
+)
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,14 +5,39 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
+	})
 }

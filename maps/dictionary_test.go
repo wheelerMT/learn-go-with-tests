6ff4552733c3c9d3e277c8ccce64187e76a55286
Matt Wheeler <wheelermt@proton.me>
2025-03-13 07:08:45 +0000

    Add functionality for adding words to dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -23,6 +23,27 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, definition)
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 

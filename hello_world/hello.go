bec5904a22aaba92b198a9114087f54c8bb9911b
Matt Wheeler <wheelermt@proton.me>
2025-05-19 20:41:19 +0100

    refactor: Add englishHelloPrefix constant

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+	return englishHelloPrefix + name
 }
 
 func main() {

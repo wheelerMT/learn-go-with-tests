9464a3c4e9df0e7df585b6dc685dc73a658d932e
Matt Wheeler <wheelermt@proton.me>
2025-05-19 20:57:09 +0100

    Refactor assert correct message to own function

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -7,18 +7,21 @@ func TestHello(t *testing.T) {
 		got := Hello("Matt")
 		want := "Hello, Matt"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }

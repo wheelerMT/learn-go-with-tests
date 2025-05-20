2e794e6d621da37c4f3d3558748920ee27bd4316
Matt Wheeler <wheelermt@proton.me>
2025-05-20 20:08:04 +0100

    Add French functionality

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -22,6 +22,12 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Pierre", "French")
+		want := "Bonjour, Pierre"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {

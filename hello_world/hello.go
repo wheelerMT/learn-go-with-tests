2b705122e6d5dfbfbe3166b4a246637bc1160a5a
Matt Wheeler <wheelermt@proton.me>
2025-05-20 20:04:06 +0100

    Add Spanish functionality

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Matt"))
+	fmt.Println(Hello("Matt", ""))
 }
